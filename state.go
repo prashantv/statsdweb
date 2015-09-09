c27882275b0eacfee314cc6c1eaab95f909b5e25
Prashant Varanasi <prashant@prashantv.com>
2015-09-08 23:02:45 -0700

    Store all timer values

--- state.go
+++ state.go
@@ -5,13 +5,16 @@ import "sync"
 type metricsState struct {
 	sync.RWMutex
 
-	Counters map[string]int64 `json:"counters"`
-	Gauges   map[string]int64 `json:"gauges"`
+	Counters map[string]int64   `json:"counters"`
+	Gauges   map[string]int64   `json:"gauges"`
+	Timers   map[string][]int64 `json:"timers"`
 }
 
 func newMetricsState() *metricsState {
 	return &metricsState{
 		Counters: make(map[string]int64),
+		Timers:   make(map[string][]int64),
+		Gauges:   make(map[string]int64),
 	}
 }
 
@@ -23,7 +26,10 @@ func (s *metricsState) IncCounter(name string, value int64) {
 }
 
 func (s *metricsState) RecordTimer(name string, value int64) {
+	s.Lock()
+	defer s.Unlock()
 
+	s.Timers[name] = append(s.Timers[name], value)
 }
 
 func (s *metricsState) UpdateGauge(name string, value int64) {
