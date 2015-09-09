88c176ebc8d6382294e924b9d5eb67a269c7190f
Prashant Varanasi <prashant@prashantv.com>
2015-09-08 23:01:57 -0700

    Implement gauge storage

--- state.go
+++ state.go
@@ -6,6 +6,7 @@ type metricsState struct {
 	sync.RWMutex
 
 	Counters map[string]int64 `json:"counters"`
+	Gauges   map[string]int64 `json:"gauges"`
 }
 
 func newMetricsState() *metricsState {
@@ -26,5 +27,8 @@ func (s *metricsState) RecordTimer(name string, value int64) {
 }
 
 func (s *metricsState) UpdateGauge(name string, value int64) {
+	s.Lock()
+	defer s.Unlock()
 
+	s.Gauges[name] = value
 }
